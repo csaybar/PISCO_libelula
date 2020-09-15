70c167ca85ff5d3880f6dee9457a2720aa70b41a
csaybar <csaybar@gmail.com>
2020-09-15 17:35:33 -0500

    pisco_lib up

--- main.R
+++ main.R
@@ -1,6 +1,7 @@
 library(raster)
 library(lubridate)
 library(RCurl)
+library(magrittr)
 library(R.utils)
 
 source("utils.R")
@@ -12,16 +13,20 @@ month_seq <- seq(as.Date("1981-01-01"), as.Date("2019-07-31"), "month")
 # 1. Create folders
 create_folders(path)
 
-# 2. Download CHIRPd
+# 2. Download CHIRPd and CHIRPm
 for (index in seq_along(day_seq)) {
   download_CHIRPd(date = day_seq[index], path)
 }
-
-# 2. Download CHIRPm
 for (index in seq_along(day_seq)) {
   download_CHIRPm(date = month_seq[index], path)
 }
 
+# 4.Test if all the necessary files are saved
+test_CHIRP(path)
+
+# 5. Create data cubes
+create_CHIRPd_netcdf(path)
+create_CHIRPm_netcdf(path)
 
 
 
